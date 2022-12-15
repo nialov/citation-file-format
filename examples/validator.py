215782452dfd184ed9460afef6cbe5ccf779d3c7
nialov <nikolasovaskainen@gmail.com>
2022-12-15 15:28:44 +0200

    feat: implement validator as pre-commit-hook

--- examples/validator.py
+++ examples/validator.py
@@ -1,32 +1,47 @@
-import jsonschema
 import argparse
-from ruamel.yaml import YAML
 import json
+from pathlib import Path
+
+import jsonschema
+from ruamel.yaml import YAML
 
 
-def validate(data_path, schema_path):
-    with open(data_path, 'r') as fi:
-        # Convert to Python object
-        yaml = YAML(typ='safe')
-        yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:timestamp'] = yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:str']
-        yml_data = yaml.load(fi)
-
-        with open(schema_path, 'r') as sf:
-            schema_data = json.loads(sf.read())
-            jsonschema.validate(instance=yml_data, schema=schema_data, format_checker=jsonschema.FormatChecker())
-
-
-if __name__ == "__main__":
+def main():
     """Run like this from the command line:
     `python3 -m validator.py -d data.yml -s schema.json`
     """
-    parser = argparse.ArgumentParser(  
-        description='Validates a YAML file against a JSON Schema')  
-    parser.add_argument(  
-        '-d', '--data', type=str,  
-        help='A YAML data file', required=True)
+    parser = argparse.ArgumentParser(
+        description="Validates a YAML file against a JSON Schema"
+    )
+    parser.add_argument("data", type=str, help="A YAML data file")
     parser.add_argument(
-        '-s', '--schema', type=str,
-        help='A JSON Schema file', required=True) 
-    args = parser.parse_args()  
+        "-s",
+        "--schema",
+        type=str,
+        help="A JSON Schema file",
+        default=(Path(__file__).parent.parent / "schema.json").as_posix(),
+    )
+    args = parser.parse_args()
     validate(args.data, args.schema)
+
+
+def validate(data_path, schema_path):
+    with open(data_path, "r") as fi:
+        # Convert to Python object
+        yaml = YAML(typ="safe")
+        yaml.constructor.yaml_constructors[
+            "tag:yaml.org,2002:timestamp"
+        ] = yaml.constructor.yaml_constructors["tag:yaml.org,2002:str"]
+        yml_data = yaml.load(fi)
+
+        with open(schema_path, "r") as sf:
+            schema_data = json.loads(sf.read())
+            jsonschema.validate(
+                instance=yml_data,
+                schema=schema_data,
+                format_checker=jsonschema.FormatChecker(),
+            )
+
+
+if __name__ == "__main__":
+    main()
