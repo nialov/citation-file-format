4c47761b859b0cb5c42d6ad7b3f931d8c2a1d6c7
nialov <nikolasovaskainen@gmail.com>
2022-12-15 16:08:39 +0200

    feat: report yaml load error separately

--- examples/validator.py
+++ examples/validator.py
@@ -7,6 +7,7 @@ import sys
 import jsonschema
 from jsonschema.exceptions import ValidationError
 from ruamel.yaml import YAML
+from ruamel.yaml.scanner import ScannerError
 
 
 def main():
@@ -38,19 +39,22 @@ def validate(data_paths:List[Path], schema_path:Path):
             yaml.constructor.yaml_constructors[
                 "tag:yaml.org,2002:timestamp"
             ] = yaml.constructor.yaml_constructors["tag:yaml.org,2002:str"]
-            yml_data = yaml.load(fi)
 
             # with open(schema_path, "r") as sf:
             with schema_path.open(mode="r") as sf:
                 schema_data = json.loads(sf.read())
                 try:
+                    yml_data = yaml.load(fi)
                     jsonschema.validate(
                         instance=yml_data,
                         schema=schema_data,
                         format_checker=jsonschema.FormatChecker(),
                     )
-                except ValidationError:
-                    print(f"ERROR: Failed to validate {data_path}\n", file=sys.stderr)
+                except ( ValidationError, ScannerError ) as err:
+                    if isinstance(err, ValidationError):
+                        print(f"ERROR: Failed to validate {data_path}\n", file=sys.stderr)
+                    else:
+                        print(f"ERROR: Failed to load yaml of {data_path}\n", file=sys.stderr)
                     raise
 
 
