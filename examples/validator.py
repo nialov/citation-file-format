7fb3dd2307c30680c7b4b1475eedd4aaca6c09f2
nialov <nikolasovaskainen@gmail.com>
2022-12-15 16:42:51 +0200

    fix: add old validate function back for tests

--- examples/validator.py
+++ examples/validator.py
@@ -11,8 +11,10 @@ from ruamel.yaml.scanner import ScannerError
 
 
 def main():
-    """Run like this from the command line:
-    `python3 -m validator.py -d data.yml -s schema.json`
+    """
+    Run like this from the command line:
+
+    `python3 -m validator.py -s schema.json CITATION.cff`
     """
     parser = argparse.ArgumentParser(
         description="Validates a YAML file against a JSON Schema"
@@ -26,10 +28,10 @@ def main():
         default=(Path(__file__).parent.parent / "schema.json").as_posix(),
     )
     args = parser.parse_args()
-    validate(args.data, args.schema)
+    validate_multiple(args.data, args.schema)
 
 
-def validate(data_paths:List[Path], schema_path:Path):
+def validate_multiple(data_paths:List[Path], schema_path:Path):
     for data_path in data_paths:
 
         with data_path.open(mode="r") as fi:
@@ -57,6 +59,16 @@ def validate(data_paths:List[Path], schema_path:Path):
                         print(f"ERROR: Failed to load yaml of {data_path}\n", file=sys.stderr)
                     raise
 
+def validate(data_path, schema_path):
+    with open(data_path, 'r') as fi:
+        # Convert to Python object
+        yaml = YAML(typ='safe')
+        yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:timestamp'] = yaml.constructor.yaml_constructors[u'tag:yaml.org,2002:str']
+        yml_data = yaml.load(fi)
+
+        with open(schema_path, 'r') as sf:
+            schema_data = json.loads(sf.read())
+            jsonschema.validate(instance=yml_data, schema=schema_data, format_checker=jsonschema.FormatChecker())
 
 
 
